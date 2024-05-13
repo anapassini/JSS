29c620a651ecd480cf3e97dcbff2b0c8d1cda3cf
ANA CLARA PASSINI DE ALMEIDA <57900693807@info.local>
2024-05-13 08:39:50 -0300

    feat: terminado exercicio 8

--- ex8.js
+++ ex8.js
@@ -10,3 +10,40 @@
 // IMC de 35.0 a 39.9: Obesidade grau 2
 // IMC maior ou igual a 40.0: Obesidade grau 3
 
+let peso = parseFloat (prompt("Informe o seu peso (em kg): "));
+
+let altura = parseFloat (prompt ("Informe a sua altura (em metros): "))
+
+let imc = peso / (altura*altura);
+
+console.log ("Seu IMC é: " + imc);
+
+if (imc < 18.5){
+
+    console.log("Abaixo do peso");
+}
+
+else if (imc >= 18.5 && imc <= 24.9){
+
+    console.log("Peso normal");
+}
+
+else if (imc >= 25 && imc <= 29.9){
+
+    console.log("Sobrepeso");
+}
+
+else if (imc >= 30.0 && imc <= 34.9){
+
+    console.log("Obsidade grau 1");
+}
+
+else if (imc >= 35.0 && imc <= 39.9){
+
+    console.log("Obsidade grau 2");
+}
+
+else {
+
+console.log ("Obsidade grau 3")
+}
