c7322564b5b58d0fc7e2fa49b003596ff3bf680d
ANA CLARA PASSINI DE ALMEIDA <57900693807@info.local>
2024-05-14 10:01:45 -0300

    feat: terminado exercicio 8

---
+++ loop/ex7 lista 2.js
@@ -0,0 +1,14 @@
+// 7. Calcular a soma dos números de 1 a 100 utilizando a estrutura
+// while.
+
+let num = 1;
+let soma = soma
+
+
+
+while (num < 100){
+    soma += num
+    num += 1
+    console.log(num)
+}
+
